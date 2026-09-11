7a953aa1c1aad8aaa76f20a86c221ec04953ef76
000 <ooo@MacBook-Pro-000.local>
2026-09-11 18:08:51 +0200

    fix school jeans price to 4990

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import Header from "@/_components/Header";
 
